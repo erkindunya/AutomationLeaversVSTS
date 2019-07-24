a70c107fd0d89d66a4f61b4a63455de18e966bd7
Gabor Vig <gabor.vig@kier.co.uk>
2019-07-24 11:56:46 +0100

    Finalised leavers template ready

--- src/webparts/automationLeavers/AutomationLeaversWebPart.ts
+++ src/webparts/automationLeavers/AutomationLeaversWebPart.ts
@@ -1,3 +1,19 @@
+// polyfills
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+// import 'core-js/es6/weak-map';
+import "@pnp/polyfill-ie11";
+// end polyfills
+
 import { Version } from '@microsoft/sp-core-library';
 import {
   BaseClientSideWebPart,
@@ -8,10 +24,17 @@ import { escape } from '@microsoft/sp-lodash-subset';
 
 import * as strings from 'AutomationLeaversWebPartStrings';
 
+require('./main.scss');
+
 // Importing Vue.js
 import Vue from 'vue';
+
+import store from './store';
+import router from './router';
 // Improting Vue.js SFC
 import AutomationLeaversComponent from './components/AutomationLeavers.vue';
+
+import { sp } from "@pnp/sp";
 
 export interface IAutomationLeaversWebPartProps {
   description: string;
@@ -23,8 +46,18 @@ export default class AutomationLeaversWebPart extends BaseClientSideWebPart<IAut
     const id: string = `wp-${this.instanceId}`;
     this.domElement.innerHTML = `<div id="${id}"></div>`;
 
+    router.beforeEach((to, from, next) => {
+      if(store.state.maxPage < parseInt(to.name)) {
+        next('/1');
+      } else {
+        next();
+      }
+    });
+
     let el = new Vue({
       el: `#${id}`,
+      store,
+      router,
       render: h => h(AutomationLeaversComponent, {
         props: {
           description: this.properties.description
@@ -57,5 +90,14 @@ export default class AutomationLeaversWebPart extends BaseClientSideWebPart<IAut
         }
       ]
     };
+  }
+  public onInit(): Promise<void> {
+
+    return super.onInit().then(_ => {
+  
+      sp.setup({
+        spfxContext: this.context
+      });
+    });
   }
 }
