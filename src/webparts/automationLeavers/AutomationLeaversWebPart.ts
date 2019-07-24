fe8e27ed34fe076f9d7aee9e676c56ff38a442c2
Gabor Vig <gabor.vig@kier.co.uk>
2019-07-24 10:20:25 +0100

    Init

---
+++ src/webparts/automationLeavers/AutomationLeaversWebPart.ts
@@ -0,0 +1,61 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import * as strings from 'AutomationLeaversWebPartStrings';
+
+// Importing Vue.js
+import Vue from 'vue';
+// Improting Vue.js SFC
+import AutomationLeaversComponent from './components/AutomationLeavers.vue';
+
+export interface IAutomationLeaversWebPartProps {
+  description: string;
+}
+
+export default class AutomationLeaversWebPart extends BaseClientSideWebPart<IAutomationLeaversWebPartProps> {
+
+  public render(): void {
+    const id: string = `wp-${this.instanceId}`;
+    this.domElement.innerHTML = `<div id="${id}"></div>`;
+
+    let el = new Vue({
+      el: `#${id}`,
+      render: h => h(AutomationLeaversComponent, {
+        props: {
+          description: this.properties.description
+        }
+      })
+    });
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/automationLeavers/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAutomationLeaversWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AutomationLeaversWebPartStrings' {
+  const strings: IAutomationLeaversWebPartStrings;
+  export = strings;
+}
